f28943729b32b696d68987af933800e33e5b024e
zosaekki <chohyunm1n@naver.com>
2022-06-20 18:05:58 +0900

    MVC pattern

--- application/controllers/BoardController.php
+++ application/controllers/BoardController.php
@@ -1,7 +1,13 @@
 <?php
+    namespace application\controllers;
 
-    use application\controllers\Controller;
+    use application\models\BoardModel;
 
     class BoardController extends Controller {
-
+        public function list() {
+            $model = new BoardModel();
+            //$this->list = $model->selBoardList();
+            $this->addAttribute("list", $model->selBoardList());
+            return "board/list.php"; // view 파일명!!
+        }
     }
